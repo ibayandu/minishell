d6d24cbd1fb205cabc2ae4d8de51d4dbee53f62a
Yasin ZEYBEK <yzeybek@ThinkPad>
2025-05-08 16:17:01 +0300

    proceed on parser with new structs and y_tab

--- src/parser/incs/parser.h
+++ src/parser/incs/parser.h
@@ -6,7 +6,7 @@
 /*   By: yzeybek <yzeybek@student.42.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 14:28:40 by ibayandu          #+#    #+#             */
-/*   Updated: 2025/05/07 16:09:56 by yzeybek          ###   ########.tr       */
+/*   Updated: 2025/05/08 16:08:28 by yzeybek          ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,64 +15,100 @@
 
 typedef struct s_command	t_command;
 
-typedef enum e_command_type
+typedef enum e_cmd_type
 {
 	CMD_SIMPLE,
 	CMD_CONNECT,
-	CMD_SUBSHELL,
+	CMD_SUBSHELL
 
-}	t_command_type;
+}	t_cmd_type;
 
-typedef enum e_connect_type
+typedef enum e_cnt_type
 {
-	CNT_PIPE
-}	t_connect_type;
+	CNT_PIPE,
+	CNT_AND_AND,
+	CNT_OR_OR
+
+}	t_cnt_type;
 
 typedef enum e_redir_type
 {
-	REDIR_OUTPUT
+	REDIR_OUTPUT,
+	REDIR_INPUT,
+	REDIR_APPEND,
+	REDIR_UNTIL,
+	REDIR_STRING,
+
 }	t_redir_type;
 
-typedef struct	s_simple_cmd
+
+typedef struct s_word
 {
-	char	**args;
-	t_redir	*redirects;
+	char	*word;
+	int		flags;
+
+}	t_word;
+
+typedef struct s_word_list
+{
+	t_word				*word;
+	struct s_word_list	*next;
+
+}	t_word_list;
+
+typedef union u_redir
+{
+	int		fd;
+	t_word	*filename;
+
+}	t_redir;
+
+typedef struct s_redirect
+{
+	int					rflags;
+	int					flags;
+	char				*here_doc_eof;
+	t_redir				redirector;
+	t_redir				redirectee;
+	t_redir_type		redir_type;
+	struct redirect		*next;
+
+}	t_redirect;
+
+typedef struct s_element
+{
+	t_word		*word;
+	t_redirect	*redirect;
+
+}	t_element;
+
+typedef struct s_simple_cmd
+{
+	int			flags;
+	t_word_list	*words;
+	t_redirect	*redirects;
 
 }	t_simple_cmd;
 
 typedef struct s_connect_cmd
 {
-	t_command		*first;
-	t_command		*second;
-	t_connect_type	connect_type;
+	t_command	*first;
+	t_command	*second;
+	t_cnt_type	type;
 
-}	t_connect_cmd; 
+}	t_connect_cmd;
 
 typedef struct s_subshell_cmd
 {
+	int			flags;
 	t_command	*command;
+
 }	t_subshell_cmd;
-
-typedef struct s_redir
-{
-	char	*filename;
-	int		fd;
-
-}	t_redir;
-
-
-typedef struct s_redirect
-{
-	t_redir			redirector;
-	t_redir			redirectee;
-	t_redir_type	redir_type;
-	struct s_redir	*next;
-
-}	t_redirect;
 
 typedef struct command
 {
-	t_command_type	type;
+	int				flags;
+	t_cmd_type		type;
 	t_redirect		*redirects;
 	union u_content
 	{
