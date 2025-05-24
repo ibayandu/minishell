f34609309b952fe5f0c4a015b266825200ed54e4
ibayandu <ibayandu@student.42istanbul.com.tr>
2025-05-24 19:15:03 +0300

    yasinin pc'den yaptigimiz deisikliklerin merge merge yaptigimiz hali.

--- src/libs/libft/srcs/ft_nbrlen.c
+++ src/libs/libft/srcs/ft_nbrlen.c
@@ -6,7 +6,7 @@
 /*   By: ibayandu <ibayandu@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 20:20:05 by yzeybek           #+#    #+#             */
-/*   Updated: 2025/05/24 19:08:19 by ibayandu         ###   ########.fr       */
+/*   Updated: 2025/05/24 19:09:35 by ibayandu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ unsigned int	ft_nbrlen(const char *str)
 	i = -1;
 	while (str[++i])
 		if (!ft_isdigit(str[i]))
-			break;
+			break ;
 	return (i);
 }
